6784473b3158554545cff0581808f49843ca9616
Yaiza Ramirez <yaizaramirezbecerra@gmail.com>
2024-11-02 19:29:08 +0000

    Updated GitHub website

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer id="contact">
       <p>Feel free to reach out to me via <a href="mailto:yaizaramirezbecerra@gmail.com">email</a>.</p>
-      <p>Find me on <a href="https://github.com/yourprofile" target="_blank" rel="noopener noreferrer">GitHub</a>.</p>
+      <p>Find me on <a href="https://github.com/yaizaramirezbecerra" target="_blank" rel="noopener noreferrer">GitHub</a>.</p>
     </footer>
   );
 }
